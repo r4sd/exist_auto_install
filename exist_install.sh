ae65d7d2e70bd6f9b09d043296af7305c7db1e37
root <root@localhost.localdomain>
2020-11-18 13:08:06 +0900

    [ADD]sed -i -e s/.split(|)//g intelligence/settings.py

--- exist_install.sh
+++ exist_install.sh
@@ -69,6 +69,7 @@ cd /opt/exist
 pip install -r requirements.txt
 
 ## Change settings.py
+sed -i -e "s/.split('|')//g" intelligence/settings.py
 sed -i -e "s/\"SET CHARACTER SET utf8mb4;\"/\"SET CHARACTER SET utf8mb4;\"\n                            \"SET sql_mode='STRICT_TRANS_TABLES';\"/g" intelligence/settings.py
 
 ## Django initial setting
@@ -78,12 +79,12 @@ python3 manage.py migrate
 ## .env initial setting
 cp .env.example .env
 SECRET_KEY=$(python3 keygen.py)
-HOST_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+#HOST_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 sed -i -e "s/your_database_user/${DBUSER_EXIST}/g" .env
 sed -i -e "s/your_database_password/${DBPASSWORD_EXIST}/g" .env
 sed -i -e "s/insert_your_secret_key/${SECRET_KEY}/g" .env
 sed -i -e "s/False/True/g" .env
-sed -i -e "s/192.168.56.101/${HOST_IP}|localhost|/g" .env
+sed -i -e "s/192.168.56.101/*/g" .env
 
 ## Redis install
 yum install redis -y
